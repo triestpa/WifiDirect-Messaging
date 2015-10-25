ed92068a259394d7489038f77adfbe26eb6313d6
triestpa <patrick.triest@gmail.com>
2015-10-25 15:15:28 -0500

    Add code initializing WifiP2p manager and channel and broadcast reciever

--- app/src/main/java/com/triestpa/wifi_direct_messaging/MainActivity.java
+++ app/src/main/java/com/triestpa/wifi_direct_messaging/MainActivity.java
@@ -1,5 +1,10 @@
 package com.triestpa.wifi_direct_messaging;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -7,11 +12,54 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    private final IntentFilter intentFilter = new IntentFilter();
+    WifiP2pManager.Channel mChannel;
+    WifiP2pManager mManager;
+    MainActivity mActivity;
+    WiFiDirectBroadcastReceiver mReceiver;
+
+    boolean wifiP2pEnabled = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //  Indicates a change in the Wi-Fi P2P status.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+
+        // Indicates a change in the list of available peers.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+
+        // Indicates the state of Wi-Fi P2P connectivity has changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+
+        // Indicates this device's details have changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+        mActivity = this;
     }
+
+    /** register the BroadcastReceiver with the intent values to be matched */
+    @Override
+    public void onResume() {
+        super.onResume();
+        mReceiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, this);
+        registerReceiver(mReceiver, intentFilter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(mReceiver);
+    }
+
+    public void setIsWifiP2pEnabled(boolean isEnabled) {
+        wifiP2pEnabled = isEnabled;
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -33,5 +81,50 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
+        WifiP2pManager.Channel mChannel;
+        WifiP2pManager mManager;
+        MainActivity mActivity;
+
+        public WiFiDirectBroadcastReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel, MainActivity activity) {
+            this.mChannel = channel;
+            this.mManager = manager;
+            this.mActivity = activity;
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+                // Determine if Wifi P2P mode is enabled or not, alert
+                // the Activity.
+                int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+                if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                    mActivity.setIsWifiP2pEnabled(true);
+                } else {
+                    mActivity.setIsWifiP2pEnabled(false);
+                }
+            } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+
+                // The peer list has changed!  We should probably do something about
+                // that.
+
+            } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+
+                // Connection state changed!  We should probably do something about
+                // that.
+
+            } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+                /*
+                MainActivityFragment fragment = (MainActivityFragment) mActivity.getFragmentManager()
+                        .findFragmentById(R.id.fragment);
+                fragment.updateThisDevice((WifiP2pDevice) intent.getParcelableExtra(
+                        WifiP2pManager.EXTRA_WIFI_P2P_DEVICE));
+                */
+
+            }
+        }
     }
 }
--- app/src/main/java/com/triestpa/wifi_direct_messaging/MainActivityFragment.java
+++ app/src/main/java/com/triestpa/wifi_direct_messaging/MainActivityFragment.java
@@ -1,5 +1,6 @@
 package com.triestpa.wifi_direct_messaging;
 
+import android.net.wifi.p2p.WifiP2pDevice;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -18,5 +19,9 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    public void updateThisDevice(WifiP2pDevice device) {
+        //TODO update the device in the UI
     }
 }
